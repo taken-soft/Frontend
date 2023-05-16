a5876be6006ea91d4c07314f116e704cbb16c034
skwogus98 <skwogus98@gmail.com>
2023-05-16 13:42:31 +0900

    add layout.vue

---
+++ client/views/component/Layout.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="layout">
+        <!-- {{ item }} -->
+        <!-- 23:10 -->
+        <div class="layoutContainer">
+            <div class="layoutContent">
+                {{ layoutSeq }}
+            </div>
+            
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    props:[
+        "layoutSeq"
+    ],
+    components:{
+
+    },
+    
+}
+</script>
+
+<style scoped>
+.layout{
+    background: white;
+    min-height: 20rem;
+    border-radius: 5px;
+    height: calc(50vh - 6rem);
+}
+.layoutContainer{
+    position: relative;
+    width: 100%;
+    padding-bottom: calc(100% * 10 / 23);
+}
+.layoutContent{
+    position: absolute;
+    background: #f5f5f5;
+    border-color: #f0f0f0;
+    border-width: 1px;
+    border-radius: 5px;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
--- client/views/pages/main/Dashboard1.vue
+++ client/views/pages/main/Dashboard1.vue
@@ -1,15 +1,6 @@
 <template>
     <div class="grid2_2">
-        <div class="layout" v-for="(item, index) in 4" :key="index">
-            <!-- {{ item }} -->
-            <!-- 23:10 -->
-            <div class="layoutContainer">
-                <div class="layoutContent">
-
-                </div>
-                
-            </div>
-        </div>
+        <Layout class="layout" v-for="(item, index) in 4" :key="index" :layout-seq="item" />
         <!-- <div :class="{ active: activeTab === 'tab1' }" @click="activeTab = 'tab1'">1</div>
         <div :class="{ active: activeTab === 'tab2' }" @click="activeTab = 'tab2'">2</div>
         <div :class="{ active: activeTab === 'tab3' }" @click="activeTab = 'tab3'">3</div>
@@ -18,11 +9,16 @@
 </template>
 
 <script>
+import Layout from '../../component/Layout.vue';
+
 export default {
     data () {
         return {
             activeTab: "tab1",
         }
+    },
+    components:{
+        Layout
     },
     methods: {
         setActiveTab(tab) {
@@ -41,28 +37,7 @@ export default {
 }
 </script>
 <style scoped>
-.layout{
-    background: white;
-    min-height: 20rem;
-    border-radius: 5px;
-    height: calc(50vh - 6rem);
-}
-.layoutContainer{
-    position: relative;
-    width: 100%;
-    padding-bottom: calc(100% * 10 / 23);
-}
-.layoutContent{
-    position: absolute;
-    background: #f5f5f5;
-    border-color: #f0f0f0;
-    border-width: 1px;
-    border-radius: 5px;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-}
+
 </style>
 
 
