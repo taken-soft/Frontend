6448be5a8e69e9cdb15c51e48285c1e632a4980f
qkrwnstlr <akfldkskgorn@gmail.com>
2023-05-17 15:12:36 +0900

    feat : create menu component [menu, menuitem, menubutton]

---
+++ client/views/layout/menu/MenuItem.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex title" @click="menuEntity.onClick">
+		<p :innerHTML="menuEntity.text"></p>
+		<img class="img-l" @click="menuEntity.onIconClick" :src="menuEntity.icon" alt="" />
+	</div>
+</template>
+<script>
+import MenuEntity from './menuEntity';
+
+export default{
+	props: {
+		menuEntity:MenuEntity
+	}
+}
+</script>
+<style scoped></style>
