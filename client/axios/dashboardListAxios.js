cfc859d9a5c6549c63eda8b470d54dee011194c2
qkrwnstlr <akfldkskgorn@gmail.com>
2023-05-23 11:10:59 +0900

    feat: Axios 구현

--- client/axios/dashboardListAxios.js
+++ client/axios/dashboardListAxios.js
@@ -5,3 +5,23 @@ import { API_SERVER_HOST } from "../../Global";
 export async function getDashboardList() {
     return axios.get("http://" + API_SERVER_HOST + "/dashboards/all");
 }
+
+export async function createDashboard(createDashboardRequestDTO) {
+  return axios.post("http://" + API_SERVER_HOST + "/dashboards/new", createDashboardRequestDTO.toJson());
+}
+
+export async function getDashboard(dashboardId) {
+  return axios.get("http://" + API_SERVER_HOST + `/dashboards/${dashboardId}`);
+}
+
+export async function saveDashboard(saveDashboardRequestDTO) {
+  return axios.post("http://" + API_SERVER_HOST + `/dashboards/save`, saveDashboardRequestDTO.toJson());
+}
+
+export async function deleteDashboard(dashboardId) {
+  return axios.delete("http://" + API_SERVER_HOST + `/dashboards/${dashboardId}`);
+}
+
+export async function getSensorData(getSeneorDataListRequestDTO) {
+  return axios.post("http://" + API_SERVER_HOST + `/sensor/data`, getSeneorDataListRequestDTO.toJson());
+}
