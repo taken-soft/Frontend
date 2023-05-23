e324153ebb9926775d2d2357c36157fe83dea79e
skwogus98 <skwogus98@gmail.com>
2023-05-23 13:42:48 +0900

    feat: add chartjs and Bar,Line chart.vue

--- client/views/component/Widgets/BarChart.vue
+++ client/views/component/Widgets/BarChart.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-        BarChart
-    </div>
+    <Bar :data="data" :options="options" />
 </template>
+
 <script>
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
+import { Bar } from "vue-chartjs";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 export default {
-    
-}
+    components: {
+        Bar,
+    },
+    data() {
+        return {
+            data: {
+                labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                datasets: [
+                    {
+                        label: "Data One",
+                        backgroundColor: "#f87979",
+                        data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11],
+                    },
+                ],
+            },
+        };
+    },
+};
 </script>
-<style scoped>
-    
-</style>
+<style scoped></style>
--- client/views/component/Widgets/LineChart.vue
+++ client/views/component/Widgets/LineChart.vue
@@ -1,13 +1,48 @@
 <template>
-    <div>
-        LineChart
-    </div>
+    <Line class="lineChart" :data="data" :options="options"/>
 </template>
+
 <script>
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from "chart.js";
+import { Line } from "vue-chartjs";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
+
 export default {
-    
-}
+    name: "App",
+    components: {
+        Line,
+    },
+    data() {
+        return {
+            data: {
+                labels: ["1", "2", "3", "4", "5", "6", "7"],
+                datasets: [
+                    {
+                        label: "데이터 1",
+                        backgroundColor: "#f87979",
+                        data: [40, 39, 10, 40, 39, 80, 40],
+                    },
+                    {
+                        label: "데이터 2",
+                        backgroundColor: "#79f879",
+                        data: [10, 29, 40, 80, 19, 70, 60],
+                    },
+                ],
+            },
+
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+            },
+        };
+    },
+};
 </script>
+
 <style scoped>
-    
+.lineChart{
+    background-color: white;
+    border: solid 1px black;
+}
 </style>
