9bf4503595b55dcd0ffd75d7eeaf04f0755be806
skwogus98 <skwogus98@gmail.com>
2023-05-25 02:35:01 +0900

    feat: MenuInputItem.vue onclick fix, css

--- client/views/layout/menu/inputItem/MenuInputItem.vue
+++ client/views/layout/menu/inputItem/MenuInputItem.vue
@@ -1,29 +1,51 @@
 <template>
-	<div class="flex title" @click="menuEntity.onClick">
-		<!-- <p :innerHTML="menuEntity.text" v-if="!menuEntity.isClicked"></p> -->
-    <!-- <div class="flex" v-if="menuEntity.isClicked"> -->
-    <div class="flex">
-      <input type="text" :placeholder="menuEntity.placeholder" :value="menuEntity.currentValue" @change="menuEntity.onValueChange"/>
+    <div class="flex title dashboardTitle" @click="onClickTitle">
+        <p :innerHTML="menuEntity.text" v-if="!menuEntity.isClicked"></p>
+        <div class="flex" v-if="menuEntity.isClicked">
+            <!-- <div class="flex"> -->
+            <input type="text" :placeholder="menuEntity.placeholder" :value="menuEntity.currentValue" @change="menuEntity.onValueChange" />
+        </div>
+        <img class="img-l" @click="menuEntity.onIconClick" :src="require(`../../../../resources/images/${menuEntity.icon}.png`)" alt="" />
     </div>
-    <img class="img-l" @click="menuEntity.onIconClick" :src="require(`../../../../resources/images/${menuEntity.icon}.png`)" alt="" />
-	</div>
 </template>
 <script>
-import MenuInputItemEntity from './MenuInputItemEntity';
+import MenuInputItemEntity from "./MenuInputItemEntity";
 
 export default {
-	props: {
-		menuEntity: MenuInputItemEntity
-	}
-}
+    props: {
+        menuEntity: MenuInputItemEntity,
+    },
+    methods:{
+        onClickTitle(){
+            this.menuEntity.onClick();
+            this.$forceUpdate();
+        }
+    }
+};
 </script>
 <style scoped>
 .img-l {
-  width: 30px;
-  height: 30px;
+    width: 30px;
+    height: 30px;
 }
 img {
-  width: 20px;
-  height: 20px;
+    width: 20px;
+    height: 20px;
+}
+input {
+    margin-top: 0;
+    font-size: 2rem;
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    border: 0.1rem solid #9eaab6;
+}
+p {
+    margin-top: 1.2rem;
+    margin-left: 0.5rem;
+}
+.dashboardTitle{
+    margin-bottom: 4rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #9eaab6;
 }
 </style>
